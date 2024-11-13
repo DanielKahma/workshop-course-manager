eb52230c3a6e954e301fe939814cab15b0716cfb
Daniel.Kahma <you@example.com>
2024-11-13 11:19:56 +0100

    first commit, done Student and Course

--- src/main/java/se/lexicon/course_manager/model/Course.java
+++ src/main/java/se/lexicon/course_manager/model/Course.java
@@ -1,6 +1,122 @@
 package se.lexicon.course_manager.model;
 
-// TODO implement model
-public class Course {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
 
+// TODO implement model
+public class Course implements Serializable {
+
+    //Fields
+
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private Collection<Student> students;
+
+    //Constructors
+
+    public Course(){
+
+    }
+    public Course(int id){
+        this.id = id;
+
+    }
+    public Course(String courseName, LocalDate startDate, int weekDuration, Collection<Student> students){
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = students;
+
+    }
+    public Course(int id, String courseName, LocalDate startDate, int weekDuration, Collection<Student> students){
+        this.id = id;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = students;
+
+    }
+
+    //Getter & Setters
+
+    public int getId() {
+        return id;
+
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    public boolean enrollStudent(Student student){
+        return true;
+    }
+
+    public boolean unrollStudent(Student student){
+        return true;
+    }
+
+    //Equals & HashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && weekDuration == course.weekDuration && Objects.equals(courseName, course.courseName)
+                && Objects.equals(startDate, course.startDate) && Objects.equals(students, course.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
 }
